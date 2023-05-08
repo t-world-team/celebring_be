b0711be6861d739c01debeec95d0ff8f446de458
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2023-05-08 14:29:06 +0900

    프로젝트 생성

---
+++ src/main/java/com/tworld/celebring/CelebringApplication.java
@@ -0,0 +1,13 @@
+package com.tworld.celebring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CelebringApplication.class, args);
+    }
+
+}
