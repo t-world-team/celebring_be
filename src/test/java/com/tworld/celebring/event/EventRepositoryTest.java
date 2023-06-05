96290ad8c5b24dadc29e5c7f7435ec0f33aa0e59
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2023-06-05 13:46:07 +0900

    #34 @RunWith 애노테이션 추가

--- src/test/java/com/tworld/celebring/event/EventRepositoryTest.java
+++ src/test/java/com/tworld/celebring/event/EventRepositoryTest.java
@@ -9,13 +9,16 @@ import com.tworld.celebring.event.model.QEvent;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 public class EventRepositoryTest {
