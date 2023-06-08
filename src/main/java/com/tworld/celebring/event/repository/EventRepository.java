70e6d8c7c83e2b6c42a4ed8dfb15c92ac71be665
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2023-06-08 15:32:58 +0900

    Entity 수정으로 인한 오류 해결

--- src/main/java/com/tworld/celebring/event/repository/EventRepository.java
+++ src/main/java/com/tworld/celebring/event/repository/EventRepository.java
@@ -6,6 +6,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tworld.celebring.event.dto.EventListDto;
 import com.tworld.celebring.event.dto.QEventListDto;
 import com.tworld.celebring.event.model.QEvent;
+import com.tworld.celebring.event.model.QEventCeleb;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
@@ -18,6 +19,7 @@ public class EventRepository {
     private final JPAQueryFactory queryFactory;
 
     QEvent e = new QEvent("e");
+    QEventCeleb ec = new QEventCeleb("ec");
 
     public List<EventListDto> findCurrentEvents(Pageable pageable) {
         return queryFactory
@@ -27,7 +29,7 @@ public class EventRepository {
                         e.startDate,
                         e.endDate,
                         e.address,
-                        e.mainImageUrl
+                        e.imageUrl
                 ))
                 .from(e)
                 .where(Expressions.currentDate().between(e.startDate, e.endDate))
@@ -47,7 +49,7 @@ public class EventRepository {
     public long findEventsCountByCeleb(Long celebId) {
         return queryFactory
                 .selectFrom(e)
-                .where(e.celebId.eq(celebId))
+                .leftJoin(ec).on(e.id.eq(ec.eventId).and(ec.celebId.eq(celebId)))
                 .fetch().size();
     }
 
@@ -59,13 +61,11 @@ public class EventRepository {
                         e.startDate,
                         e.endDate,
                         e.address,
-                        e.mainImageUrl
+                        e.imageUrl
                 ))
                 .from(e)
-                .where(
-                        e.celebId.eq(celebId)
-                                .and(Expressions.currentDate().between(e.startDate, e.endDate))
-                )
+                .leftJoin(ec).on(e.id.eq(ec.eventId).and(ec.celebId.eq(celebId)))
+                .where(Expressions.currentDate().between(e.startDate, e.endDate))
                 .orderBy(e.startDate.asc())
                 .fetch();
     }
@@ -78,13 +78,11 @@ public class EventRepository {
                         e.startDate,
                         e.endDate,
                         e.address,
-                        e.mainImageUrl
+                        e.imageUrl
                 ))
                 .from(e)
-                .where(
-                        e.celebId.eq(celebId)
-                                .and(Expressions.currentDate().lt(e.startDate))
-                )
+                .leftJoin(ec).on(e.id.eq(ec.eventId).and(ec.celebId.eq(celebId)))
+                .where(Expressions.currentDate().lt(e.startDate))
                 .orderBy(e.startDate.asc())
                 .fetch();
     }
@@ -97,13 +95,11 @@ public class EventRepository {
                         e.startDate,
                         e.endDate,
                         e.address,
-                        e.mainImageUrl
+                        e.imageUrl
                 ))
                 .from(e)
-                .where(
-                        e.celebId.eq(celebId)
-                                .and(Expressions.currentDate().gt(e.endDate))
-                )
+                .leftJoin(ec).on(e.id.eq(ec.eventId).and(ec.celebId.eq(celebId)))
+                .where(Expressions.currentDate().gt(e.endDate))
                 .orderBy(e.endDate.asc())
                 .fetch();
     }
--- src/test/java/com/tworld/celebring/event/EventRepositoryTest.java
+++ src/test/java/com/tworld/celebring/event/EventRepositoryTest.java
@@ -4,8 +4,8 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tworld.celebring.event.dto.EventListDto;
 import com.tworld.celebring.event.dto.QEventListDto;
-import com.tworld.celebring.event.model.Event;
 import com.tworld.celebring.event.model.QEvent;
+import com.tworld.celebring.event.model.QEventCeleb;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,137 +22,121 @@ public class EventRepositoryTest {
 
     @Autowired JPAQueryFactory queryFactory;
 
-//    @Test
-//    void startQueryDsl() {
-//        QEvent e = new QEvent("e");
-//
-//        List<Event> events = queryFactory
-//                .select(e)
-//                .from(e)
-//                .where(e.celebId.eq((long) 2))
-//                .fetch();
-//
-//        assertThat(events.size()).isEqualTo(3);
-//    }
-//
-//    @Test
-//    @DisplayName("현재 이벤트 목록")
-//    void currentEvent() {
-//        QEvent e = new QEvent("e");
-//
-//        List<EventListDto> events = queryFactory
-//                .select(new QEventListDto(
-//                        e.id,
-//                        e.name,
-//                        e.startDate,
-//                        e.endDate,
-//                        e.address,
-//                        e.mainImageUrl
-//                ))
-//                .from(e)
-//                .where(Expressions.currentDate().between(e.startDate, e.endDate))
-//                .offset(0)  // 시작 인덱스
-//                .limit(5)   // 개수
-//                .orderBy(e.startDate.asc())
-//                .fetch();
-//
-////        assertThat(events.size()).isEqualTo(2);
-//        for (EventListDto dto : events) {
-//            System.out.println(dto.getId() + " | " + dto.getName() + " | " + dto.getStartDate() + "~" + dto.getEndDate());
-//        }
-//    }
-//
-//    @Test
-//    @DisplayName("셀럽의 현재 이벤트 목록")
-//    void currentEventByCeleb() {
-//        Long searchCelebId = 2l;
-//
-//        QEvent e = new QEvent("e");
-//
-//        List<EventListDto> events = queryFactory
-//                .select(new QEventListDto(
-//                        e.id,
-//                        e.name,
-//                        e.startDate,
-//                        e.endDate,
-//                        e.address,
-//                        e.mainImageUrl
-//                ))
-//                .from(e)
-//                .where(
-//                        e.celebId.eq(searchCelebId)
-//                                .and(Expressions.currentDate().between(e.startDate, e.endDate))
-//                )
-//                .offset(0)  // 시작 인덱스
-//                .limit(5)   // 개수
-//                .orderBy(e.startDate.asc())
-//                .fetch();
-//
-////        assertThat(events.size()).isEqualTo(2);
-//        for (EventListDto dto : events) {
-//            System.out.println(dto.getId() + " | " + dto.getName() + " | " + dto.getStartDate() + "~" + dto.getEndDate());
-//        }
-//    }
-//
-//    @Test
-//    @DisplayName("셀럽의 예정 이벤트 목록")
-//    void upcomingEventByCeleb() {
-//        Long searchCelebId = 2l;
-//
-//        QEvent e = new QEvent("e");
-//
-//        List<EventListDto> events = queryFactory
-//                .select(new QEventListDto(
-//                        e.id,
-//                        e.name,
-//                        e.startDate,
-//                        e.endDate,
-//                        e.address,
-//                        e.mainImageUrl
-//                ))
-//                .from(e)
-//                .where(
-//                        e.celebId.eq(searchCelebId)
-//                            .and(Expressions.currentDate().lt(e.startDate))
-//                )
-//                .offset(0)  // 시작 인덱스
-//                .limit(10)   // 개수
-//                .orderBy(e.startDate.asc())
-//                .fetch();
-//
-//        for (EventListDto dto : events) {
-//            System.out.println(dto.getId() + " | " + dto.getName() + " | " + dto.getStartDate() + "~" + dto.getEndDate());
-//        }
-//    }
-//
-//    @Test
-//    @DisplayName("셀럽의 지난 이벤트 목록")
-//    void pastEventByCeleb() {
-//        Long searchCelebId = 2l;
-//
-//        QEvent e = new QEvent("e");
-//
-//        List<EventListDto> events = queryFactory
-//                .select(new QEventListDto(
-//                        e.id,
-//                        e.name,
-//                        e.startDate,
-//                        e.endDate,
-//                        e.address,
-//                        e.mainImageUrl
-//                ))
-//                .from(e)
-//                .where(
-//                        e.celebId.eq(searchCelebId)
-//                                .and(Expressions.currentDate().gt(e.endDate))
-//                )
-//                .offset(0)  // 시작 인덱스
-//                .limit(10)   // 개수
-//                .orderBy(e.endDate.asc())
-//                .fetch();
-//
-//        for (EventListDto dto : events) {
-//            System.out.println(dto.getId() + " | " + dto.getName() + " | " + dto.getStartDate() + "~" + dto.getEndDate());
-//        }
-//    }
+    @Test
+    @DisplayName("현재 이벤트 목록")
+    void currentEvent() {
+        QEvent e = new QEvent("e");
+
+        List<EventListDto> events = queryFactory
+                .select(new QEventListDto(
+                        e.id,
+                        e.name,
+                        e.startDate,
+                        e.endDate,
+                        e.address,
+                        e.imageUrl
+                ))
+                .from(e)
+                .where(Expressions.currentDate().between(e.startDate, e.endDate))
+                .offset(0)  // 시작 인덱스
+                .limit(5)   // 개수
+                .orderBy(e.startDate.asc())
+                .fetch();
+
+//        assertThat(events.size()).isEqualTo(2);
+        for (EventListDto dto : events) {
+            System.out.println(dto.getId() + " | " + dto.getName() + " | " + dto.getStartDate() + "~" + dto.getEndDate());
+        }
+    }
+
+    @Test
+    @DisplayName("셀럽의 현재 이벤트 목록")
+    void currentEventByCeleb() {
+        Long searchCelebId = 2l;
+
+        QEvent e = new QEvent("e");
+        QEventCeleb ec = new QEventCeleb("ec");
+
+        List<EventListDto> events = queryFactory
+                .select(new QEventListDto(
+                        e.id,
+                        e.name,
+                        e.startDate,
+                        e.endDate,
+                        e.address,
+                        e.imageUrl
+                ))
+                .from(e)
+                .leftJoin(ec).on(e.id.eq(ec.eventId).and(ec.celebId.eq(searchCelebId)))
+                .where(Expressions.currentDate().between(e.startDate, e.endDate))
+                .offset(0)  // 시작 인덱스
+                .limit(5)   // 개수
+                .orderBy(e.startDate.asc())
+                .fetch();
+
+//        assertThat(events.size()).isEqualTo(2);
+        for (EventListDto dto : events) {
+            System.out.println(dto.getId() + " | " + dto.getName() + " | " + dto.getStartDate() + "~" + dto.getEndDate());
+        }
+    }
+
+    @Test
+    @DisplayName("셀럽의 예정 이벤트 목록")
+    void upcomingEventByCeleb() {
+        Long searchCelebId = 2l;
+
+        QEvent e = new QEvent("e");
+        QEventCeleb ec = new QEventCeleb("ec");
+
+        List<EventListDto> events = queryFactory
+                .select(new QEventListDto(
+                        e.id,
+                        e.name,
+                        e.startDate,
+                        e.endDate,
+                        e.address,
+                        e.imageUrl
+                ))
+                .from(e)
+                .leftJoin(ec).on(e.id.eq(ec.eventId).and(ec.celebId.eq(searchCelebId)))
+                .where(Expressions.currentDate().lt(e.startDate))
+                .offset(0)  // 시작 인덱스
+                .limit(10)   // 개수
+                .orderBy(e.startDate.asc())
+                .fetch();
+
+        for (EventListDto dto : events) {
+            System.out.println(dto.getId() + " | " + dto.getName() + " | " + dto.getStartDate() + "~" + dto.getEndDate());
+        }
+    }
+
+    @Test
+    @DisplayName("셀럽의 지난 이벤트 목록")
+    void pastEventByCeleb() {
+        Long searchCelebId = 2l;
+
+        QEvent e = new QEvent("e");
+        QEventCeleb ec = new QEventCeleb("ec");
+
+        List<EventListDto> events = queryFactory
+                .select(new QEventListDto(
+                        e.id,
+                        e.name,
+                        e.startDate,
+                        e.endDate,
+                        e.address,
+                        e.imageUrl
+                ))
+                .from(e)
+                .leftJoin(ec).on(e.id.eq(ec.eventId).and(ec.celebId.eq(searchCelebId)))
+                .where(Expressions.currentDate().gt(e.endDate))
+                .offset(0)  // 시작 인덱스
+                .limit(10)   // 개수
+                .orderBy(e.endDate.asc())
+                .fetch();
+
+        for (EventListDto dto : events) {
+            System.out.println(dto.getId() + " | " + dto.getName() + " | " + dto.getStartDate() + "~" + dto.getEndDate());
+        }
+    }
 }
